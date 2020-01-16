3dcbd569b617c0df9a3d2ac616bec8d07cdabf22
Gabriel Lee <gabriellee@gabriels-air.wifi.bcit.ca>
2020-01-16 15:57:04 -0800

    Fix typo

--- generate_avalanche_result.sh
+++ generate_avalanche_result.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while read p; do echo "$p" | openssl aes-128-cbc -K 00000000000000000000000000000000 -iv 00000000000000000000000000000000 | xxd -p -c 256 >> pthash; done < plaintext_list
-while read p; do echo "ooooooooooooooooooooooooooooo" | openssl aes-128-cbc -K $p -iv 00000000000000000000000000000000 | xxd -p -c 256 >> khash; done < key_list
-python avalancheeffect.py -i pthash -o pt_avalance.csv
-python avalancheeffect.py -i khash -o k_avalanche.csv
+while read p; do echo "$p" | openssl aes-128-cbc -K 00000000000000000000000000000000 -iv 00000000000000000000000000000000 | xxd -p -c 256 >> test_data/pthash; done < test_data/plaintext_list
+while read p; do echo "ooooooooooooooooooooooooooooo" | openssl aes-128-cbc -K $p -iv 00000000000000000000000000000000 | xxd -p -c 256 >> test_data/khash; done < test_data/key_list
+python avalancheeffect.py -i test_data/pthash -o test_data/pt_avalanche.csv
+python avalancheeffect.py -i test_data/khash -o test_data/k_avalanche.csv
